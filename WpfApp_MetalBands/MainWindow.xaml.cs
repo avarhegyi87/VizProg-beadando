dda9c8fea2fe4ea38ee0c5607b961ffbb245aa44
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-05-10 13:13:14 +0200

    Saving and adding bands works, genre combobox works.

--- WpfApp_MetalBands/MainWindow.xaml.cs
+++ WpfApp_MetalBands/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace enMetalBands {
     public partial class enMetalBand {
         public virtual int? NoOfAlbums => Albums?.Count;
 
-        public virtual decimal? AvgRating => Math.Round((decimal)Albums.Select(p => p.Album_rating).Average(), 2);
+        public virtual decimal? AvgRating => Albums?.Count == 0 ? 0 : Math.Round((decimal)Albums.Select(p => p.Album_rating).Average(), 2);
 
         public virtual int? NoOfBandMembers => Musicians?.Count;
 
@@ -82,12 +82,12 @@ namespace WpfApp_MetalBands {
         //}
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            hideAllGrids();
+            miListBands_Click(sender, e);
+        }
 
-            var result = context.enMetalBands.Include(x => x.Genres).Include(x => x.Albums).Include(x => x.Musicians).ToList();
-
-            dgBands.Visibility = Visibility.Visible;
-            dgBands.ItemsSource = result;
+        private void grBand_Loaded(object sender, RoutedEventArgs e) {
+            var ds = (from x in context.enGenres select x.Genre_name).ToList();
+            cbGenre.ItemsSource = ds;
         }
 
         private void miExit_Click(object sender, RoutedEventArgs e) {
@@ -134,12 +134,17 @@ namespace WpfApp_MetalBands {
         private void cbBandName_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             var b = ((ComboBox)sender).SelectedItem as enMetalBand;
             tbBandName.Text = b.Band_name;
-            cbGenre.SelectedItem = b.GenreName;
+            if (b.GenreName is null) {
+                var gn = (from x in context.enGenres where x.Genre_id == b.Genre_id select x.Genre_name).ToList();
+                cbGenre.SelectedItem = gn[0];
+            } else {
+                cbGenre.SelectedItem = b.GenreName;
+            }
             tbYoF.Text = b.Date_founding.ToString();
         }
 
         private void btBack_Click(object sender, RoutedEventArgs e) {
-            grBand.Visibility = Visibility.Collapsed;
+            miListBands_Click(sender, e);
         }
 
         private void hideAllGrids() {
@@ -181,26 +186,62 @@ namespace WpfApp_MetalBands {
             } else {
                 mb.Band_name = tbBandName.Text;
                 mb.Date_founding = yr;
-                var id = context.enGenres.Where(x => x.Genre_name == cbGenre.Text).Select(x => x.Genre_id).ToList();
+                var gid = context.enGenres.Where(x => x.Genre_name == cbGenre.Text).Select(x => x.Genre_id).ToList();
                 //var id = (from x in context.enGenres
                 //          where x.Genre_name == cbGenre.Text
                 //          select x.Genre_id).ToList();
-                mb.Genre_id = id[0];
+                mb.Genre_id = gid[0];
 
                 context.enMetalBands.Update(mb);
                 context.SaveChanges();
-
-            }
-
-            
+                MessageBox.Show("Changes saved");
+            }  
         }
 
         private void btSaveAsNewBand_Click(object sender, RoutedEventArgs e) {
+            if (tbBandName.Text.Length == 0) {
+                MessageBox.Show("Enter a band name.");
+                return;
+            } else {
+                var id = (from x in context.enMetalBands
+                          where x.Band_name == tbBandName.Text
+                          select x).FirstOrDefault();
+                if (id != null) {
+                    MessageBox.Show("There is already a band with this name. " +
+                        "If you wish to update the existing one, press \"Save\" instead of \"Save As New\", " +
+                        "otherwise provide a different band name");
+                    return;
+                }
+            }
 
+            var res = int.TryParse(tbYoF.Text, out int yr);
+
+            if (!res || (res && (yr < 1950 || yr > DateTime.Today.Year))) {
+                MessageBox.Show("Provide a valid date - it should be between 1950 and " + DateTime.Today.Year);
+                return;
+            }
+
+            var gid = context.enGenres.Where(x => x.Genre_name == cbGenre.Text).Select(x => x.Genre_id).ToList();
+
+            var new_band = new enMetalBand {
+                Band_name = tbBandName.Text,
+                Genre_id = gid[0],
+                Date_founding = yr
+            };
+
+            context.enMetalBands.Add(new_band);
+            context.SaveChanges();
+            MessageBox.Show("Band saved");
+            miListBands_Click(sender, e);
         }
 
         private void cbGenre_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 
         }
+
+        private void cbGenre_DropDownOpened(object sender, EventArgs e) {
+
+        }
+
     }
 }
